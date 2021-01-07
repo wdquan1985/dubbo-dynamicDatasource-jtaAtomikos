30fa5decaf1ef91332ec45d02f44bfe7d580cf39
wdquan1985 <460283247@qq.com>
2021-01-07 11:10:09 +0800

    Update tables-slave.sql

--- dubbo-example-provider/sql/tables-slave.sql
+++ dubbo-example-provider/sql/tables-slave.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `test_user`;
 
 CREATE TABLE `test_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL COMMENT '姓名',
+  `name` varchar(32) NOT NULL COMMENT '用户名',
   `phone` varchar(32) DEFAULT NULL COMMENT '手机号',
   `title` varchar(32) DEFAULT NULL COMMENT '职称职别',
   `email` varchar(128) DEFAULT NULL COMMENT '邮箱',
@@ -23,7 +23,7 @@ CREATE TABLE `test_user` (
 DROP TABLE IF EXISTS `test_product`;
 CREATE TABLE `test_product` (
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(32) NOT NULL COMMENT '姓名',
+	`name` VARCHAR(32) NOT NULL COMMENT '产品名称',
 	`weight` VARCHAR(32) NULL DEFAULT NULL COMMENT '宽度',
 	`size` VARCHAR(32) NULL DEFAULT NULL COMMENT '数量',
 	`height` VARCHAR(128) NULL DEFAULT NULL COMMENT '高度',
@@ -35,5 +35,5 @@ CREATE TABLE `test_product` (
 
 /*Data for the table `test_user` */
 
-insert  into `test_user`(`id`,`name`,`phone`,`title`,`email`,`gender`,`date_of_birth`,`deleted`,`sys_create_time`,`sys_create_user`,`sys_update_time`,`sys_update_user`,`record_version`) values (3520,'刘**','13800000000',NULL,NULL,'1','1984-09-25',0,'2019-03-28 14:53:44','admin','2019-05-31 11:17:55','admin',0),(6999,'张*','13800000000','TEST','test@qq.com','1','1990-06-10',0,'2019-03-28 14:53:44','admin','2019-05-31 11:17:55','admin',0),(7003,'test_add1','13800000000','TEST1','test1@qq.com','1','1947-11-01',0,'2019-06-04 23:17:48',NULL,'2019-11-07 10:40:43',NULL,0),(7004,'test_add2','13800000000','test2','test2-dsa@qq.com','1','1958-07-01',0,'2019-06-04 23:17:48',NULL,'2019-11-07 11:13:06',NULL,0);
+insert  into `test_user`(`id`,`name`,`phone`,`title`,`email`,`gender`,`date_of_birth`,`deleted`,`sys_create_time`,`sys_create_user`,`sys_update_time`,`sys_update_user`,`record_version`) values (3000,'刘某','13800000000',NULL,NULL,'1','1987-09-25',0,'2019-03-28 14:53:44','admin','2019-05-31 11:17:55','admin',0),(3001,'张某','13800000000','TEST','test@qq.com','1','1988-06-10',0,'2019-03-28 14:53:44','admin','2019-05-31 11:17:55','admin',0),(3002,'test_add1','13800000000','TEST1','test1@qq.com','1','1947-11-01',0,'2019-06-04 23:17:48',NULL,'2019-11-07 10:40:43',NULL,0),(3004,'test_add2','13800000000','test2','test2-dsa@qq.com','1','1958-07-01',0,'2019-06-04 23:17:48',NULL,'2019-11-07 11:13:06',NULL,0);
 
