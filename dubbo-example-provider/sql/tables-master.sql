780eeaf1df4f67b2036f03f197dae5ab4160ac18
wdquan1985 <460283247@qq.com>
2021-01-07 11:08:26 +0800

    Update tables-master.sql

--- dubbo-example-provider/sql/tables-master.sql
+++ dubbo-example-provider/sql/tables-master.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `test_user`;
 
 CREATE TABLE `test_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL COMMENT '姓名',
+  `name` varchar(32) NOT NULL COMMENT '用户名',
   `phone` varchar(32) DEFAULT NULL COMMENT '手机号',
   `title` varchar(32) DEFAULT NULL COMMENT '职称职别',
   `email` varchar(128) DEFAULT NULL COMMENT '邮箱',
@@ -23,7 +23,7 @@ CREATE TABLE `test_user` (
 DROP TABLE IF EXISTS `test_product`;
 CREATE TABLE `test_product` (
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(32) NOT NULL COMMENT '姓名',
+	`name` VARCHAR(32) NOT NULL COMMENT '产品名称',
 	`weight` VARCHAR(32) NULL DEFAULT NULL COMMENT '宽度',
 	`size` VARCHAR(32) NULL DEFAULT NULL COMMENT '数量',
 	`height` VARCHAR(128) NULL DEFAULT NULL COMMENT '高度',
@@ -35,5 +35,5 @@ CREATE TABLE `test_product` (
 
 /*Data for the table `test_user` */
 
-insert  into `test_user`(`id`,`name`,`phone`,`title`,`email`,`gender`,`date_of_birth`,`deleted`,`sys_create_time`,`sys_create_user`,`sys_update_time`,`sys_update_user`,`record_version`) values (3497,'王**','13800000000','test','test1@qq.com','1','1958-07-01',0,'2019-03-28 14:53:44','admin','2019-05-31 11:17:55','admin',0),(3500,'林**','13800000000',NULL,NULL,'1',NULL,0,'2019-03-28 14:53:44','admin','2019-05-31 11:21:47','admin',0),(3508,'高**','13800000000','test',NULL,'2','1978-05-20',0,'2019-03-28 14:53:44','admin','2019-05-31 11:17:55','admin',0),(7005,'test4',NULL,'test4','test4@qq.com','1',NULL,0,'2020-01-06 16:19:23',NULL,'2020-01-07 11:29:10','admin',0),(7006,'test5',NULL,'new title','test5@qq.com','1',NULL,0,'2020-01-06 16:48:52',NULL,'2020-01-07 11:19:48','admin',0),(7007,'test6',NULL,NULL,'test6@qq.com',NULL,NULL,1,'2020-01-06 16:48:52',NULL,'2020-01-07 09:46:23',NULL,0);
+insert  into `test_user`(`id`,`name`,`phone`,`title`,`email`,`gender`,`date_of_birth`,`deleted`,`sys_create_time`,`sys_create_user`,`sys_update_time`,`sys_update_user`,`record_version`) values (1000,'王某','13600000000','test','test1@qq.com','1','1985-07-01',0,'2019-03-28 14:53:44','admin','2019-05-31 11:17:55','admin',0),(1001,'林某','13600000000',NULL,NULL,'1',NULL,0,'2019-03-28 14:53:44','admin','2019-05-31 11:21:47','admin',0),(1002,'朱某','13600000000','test',NULL,'2','1986-05-20',0,'2019-03-28 14:53:44','admin','2019-05-31 11:17:55','admin',0),(1005,'test4',NULL,'test4','test4@qq.com','1',NULL,0,'2020-01-06 16:19:23',NULL,'2020-01-07 11:29:10','admin',0),(1006,'test5',NULL,'new title','test5@qq.com','1',NULL,0,'2020-01-06 16:48:52',NULL,'2020-01-07 11:19:48','admin',0),(1007,'test6',NULL,NULL,'test6@qq.com',NULL,NULL,1,'2020-01-06 16:48:52',NULL,'2020-01-07 09:46:23',NULL,0);
 
